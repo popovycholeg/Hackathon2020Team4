ff11562307483b87db0c12f873fd07f4caed434e
Oleg Popovych <magmoba@gmail.com>
2020-01-31 20:24:24 +0200

    Added project

---
+++ Hackathon2020Team4/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hackathon2020Team4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
