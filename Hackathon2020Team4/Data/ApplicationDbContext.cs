0eba5c718a350d9e19bce5655efc78ed9c161cb2
Oleg Popovych <magmoba@gmail.com>
2020-02-04 00:02:46 +0200

    Models

--- Hackathon2020Team4/Data/ApplicationDbContext.cs
+++ Hackathon2020Team4/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Hackathon2020Team4.Models;
 
 namespace Hackathon2020Team4.Data
 {
@@ -13,5 +14,13 @@ namespace Hackathon2020Team4.Data
         {
 
         }
+
+        public DbSet<ApplicationUser> Users1 { get; set; } // TODO: взяти таблицю Users з Identity
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<ModuleRating> ModuleRating { get; set; }
     }
 }
---
+++ Hackathon2020Team4/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace Hackathon2020Team4.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [DisplayName("Університет")]
+        public string Institution { get; set; }
+        [DisplayName("Факультет")]
+        public string Faculty { get; set; }
+        [DisplayName("Курс навчання")]
+        public int InstitutionCourse { get; set; }
+        [DisplayName("Про себе")]
+        public string AboutMe { get; set; }
+        [DataType("Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        public string ApplicationUserID { get; set; }
+        public ApplicationUser User { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
