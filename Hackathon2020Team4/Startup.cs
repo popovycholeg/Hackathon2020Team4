fe7dc55a386251ca7ee0001e938bc226c36923c3
Oleg Popovych <magmoba@gmail.com>
2020-02-04 23:31:39 +0200

    RolesController

--- Hackathon2020Team4/Startup.cs
+++ Hackathon2020Team4/Startup.cs
@@ -31,7 +31,7 @@ namespace Hackathon2020Team4
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddControllersWithViews();
