f7fcd63bacac8aeebf1d03e4d2318b4ac918412b
AndreyBest <andrey255@live.com>
2020-02-06 22:24:59 +0200

    Add student cabinet views

---
+++ Hackathon2020Team4/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hackathon2020Team4.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hackathon2020Team4.Controllers
+{
+    public class StudentController : Controller
+    {
+        // private ApplicationDbContext _context;
+        //public StudentController(ApplicationDbContext context)
+        //{
+        //    _context = context;
+        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/Course/{id}")]
+        public IActionResult Course(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Marks()
+        {
+            return View();
+        }
+
+        public IActionResult Rate()
+        {
+            return View();
+        }
+    }
+}
